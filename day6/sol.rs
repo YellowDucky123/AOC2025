a36d913fbeb0b1bc3efe0d25b04ce0fad312488d
kelvin <kelvinjyoga@gmail.com>
2025-12-19 19:04:30 +1100

    day2 p2 construction

--- day6/sol.rs
+++ day6/sol.rs
@@ -1,8 +1,11 @@
 use std::fs;
-const FILE_PATH : &str = "./input.txt";
+const FILE_PATH : &str = "./test.txt";
 fn main () {
     let contents = fs::read_to_string(FILE_PATH).expect("Should have been able to read file");
 
+    let lines_part2 : Vec<&str> = contents.lines().collect();
+
+    println!("part2 : {:?}", lines_part2);
     
     let lines: Vec<Vec<&str>> = contents.lines() 
                                         .map(|line| {
@@ -16,6 +19,56 @@ fn main () {
     println!("part 1 : {}", part1_grand_total);
 }
 
+fn part2(lines : Vec<&str>) -> u64 {
+    let mut grand_total : u64 = 0;
+
+    let width = lines[0].len();
+    let height = lines.len() - 1;
+
+    let mut start_of_equation : bool = true;
+
+    let mut curr_sign = "";
+    let mut curr_equation_size = 1;
+    let mut curr_eq_idx = 0;
+    let mut curr_total : u64 = 0;
+    for w in 0..width {
+        if start_of_equation {
+            curr_sign = lines[lines.len() - 1][w];
+            
+            let mut i = w + 1;
+            while lines[lines.len() - 1][i] == " " {
+                curr_equation_size += 1;
+            } 
+            curr_equation_size = i;
+
+            start_of_equation = false;
+        }
+        
+        if curr_eq_idx == curr_equation_size {
+            curr_equation_size = 1;
+            curr_eq_idx = 0;
+            grand_total += curr_total;
+            curr_total = 0;
+            continue;
+        }
+        
+        let mut curr_number = 0;
+        for h in 0..height {
+            if lines[h][w] != " " {
+                curr_number *= 10;
+                curr_number += match lines[h][w].parse::<u64>() {
+                    Ok(number) => number,
+                    Err(_e) => {
+                        println!("p2 Failed to parse {}", lines[h][w]);
+                        return 0;
+                    }
+                };
+            }
+        }
+
+        curr_total += curr_number;
+    }
+}
 
 fn part1(lines : Vec<Vec<&str>>) -> u64 {
     let mut grand_total : u64 = 0;
@@ -49,7 +102,7 @@ fn part1(lines : Vec<Vec<&str>>) -> u64 {
                 total *= curr_num;
             }
         }
-        println!("total {}", total);
+        //println!("total {}", total);
         grand_total += total;
     }
     return grand_total;
