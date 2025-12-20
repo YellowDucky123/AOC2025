1235ae7c66b462829d4e25afa2b16e0c9ce28a7b
kelvin <kelvinjyoga@gmail.com>
2025-12-20 16:42:27 +1100

    day6 fin

--- day6/sol.rs
+++ day6/sol.rs
@@ -1,12 +1,10 @@
 use std::fs;
-const FILE_PATH : &str = "./test.txt";
+const FILE_PATH : &str = "./input.txt";
 fn main () {
     let contents = fs::read_to_string(FILE_PATH).expect("Should have been able to read file");
 
     let lines_part2 : Vec<&str> = contents.lines().collect();
 
-    println!("part2 : {:?}", lines_part2);
-    
     let lines: Vec<Vec<&str>> = contents.lines() 
                                         .map(|line| {
                                             line.split_whitespace()             
@@ -14,9 +12,11 @@ fn main () {
                                         })
                                         .filter(|v: &Vec<&str>| !v.is_empty())     
                                         .collect();                 
-    println!("{:?}", lines);
     let part1_grand_total = part1(lines.clone());
     println!("part 1 : {}", part1_grand_total);
+
+    let part2_grand_total = part2(lines_part2.clone());
+    println!("part 2 : {}", part2_grand_total);
 }
 
 fn part2(lines : Vec<&str>) -> u64 {
@@ -27,47 +27,59 @@ fn part2(lines : Vec<&str>) -> u64 {
 
     let mut start_of_equation : bool = true;
 
-    let mut curr_sign = "";
-    let mut curr_equation_size = 1;
+    let mut curr_sign : u8 = b' ';
+    let mut curr_equation_size = 0;
     let mut curr_eq_idx = 0;
     let mut curr_total : u64 = 0;
     for w in 0..width {
         if start_of_equation {
-            curr_sign = lines[lines.len() - 1][w];
+            curr_sign = lines[lines.len() - 1].as_bytes()[w];
+
+            if curr_sign == b'*' {
+                curr_total = 1;
+            }
             
             let mut i = w + 1;
-            while lines[lines.len() - 1][i] == " " {
+            while i < width && lines[lines.len() - 1].as_bytes()[i] == b' ' {
                 curr_equation_size += 1;
+                i += 1;
             } 
-            curr_equation_size = i;
+
+            if i == width {
+                curr_equation_size += 1;
+            }
 
             start_of_equation = false;
         }
-        
+
         if curr_eq_idx == curr_equation_size {
-            curr_equation_size = 1;
+            curr_equation_size = 0;
             curr_eq_idx = 0;
             grand_total += curr_total;
             curr_total = 0;
+            start_of_equation = true;
             continue;
         }
-        
-        let mut curr_number = 0;
+         
+        let mut curr_number : u64 = 0;
         for h in 0..height {
-            if lines[h][w] != " " {
-                curr_number *= 10;
-                curr_number += match lines[h][w].parse::<u64>() {
-                    Ok(number) => number,
-                    Err(_e) => {
-                        println!("p2 Failed to parse {}", lines[h][w]);
-                        return 0;
-                    }
-                };
+            if lines[h].as_bytes()[w] != b' ' {
+                curr_number *= 10_u64;
+                curr_number += (lines[h].as_bytes()[w] - b'0') as u64;
             }
         }
 
-        curr_total += curr_number;
+        if curr_sign == b'+' {
+            curr_total += curr_number;
+        } else if curr_sign == b'*' {
+            curr_total *= curr_number;
+        }
+        curr_eq_idx += 1;   
     }
+
+    grand_total += curr_total;
+
+    return grand_total;
 }
 
 fn part1(lines : Vec<Vec<&str>>) -> u64 {
